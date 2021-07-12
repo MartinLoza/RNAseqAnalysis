e9c754680311b3ae29f0aaf81c65a5584c81a5ed
MARTIN LOZA <mloza@ifrec.osaka-u.ac.jp>
2021-07-12 16:59:09 +0900

    Sample data.

--- R/Various.R
+++ R/Various.R
@@ -12,3 +12,21 @@ SeuratPreprocessing <- function(object = NULL, verbose = FALSE, ...){
 
   return(object)
 }
+
+#' Title
+#'
+#' @param object Object to sample
+#' @param frac Fraction of samples. Must be a value between 0 and 1.
+#' @param seed Seed to use on sampling
+#' @param ... Arguments passed to other methods.
+#'
+#' @return Sampled data
+#' @export
+SampleData <- function(object = NULL, frac = NULL, seed = 777, ...){
+  set.seed(seed)
+  nCells <- ncol(object)
+  samples <- floor(frac*nCells)
+  idx <- sample(x = nCells, size = samples, ... )
+
+  return(object[,idx])
+}
