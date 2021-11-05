0abaa695cf283bec5dc6879a66d599de77856790
MARTIN LOZA <mloza@ifrec.osaka-u.ac.jp>
2021-11-05 16:59:45 +0900

    Add PlotKEEG function.

--- R/PathwayEnrichment.R
+++ R/PathwayEnrichment.R
@@ -64,3 +64,48 @@ GetTopKEGG <- function(df = NULL, top.by = "Selected_PV", type = "double" , ntop
 
   return(tmp)
 }
+
+#' PlotKEGG
+#'
+#' @param df Data frame containing KEGG pathways
+#' @param type Type of selected p-values. Available options are: "double", negatives and positive, "positive", and "negative".
+#' @param ntop Number of top values.
+#' @param color_by Label to color points.
+#' @param point_size Size of points
+#' @param text_size Size of text in the plot.
+#'
+#' @return A ggplot object.
+#' @export
+PlotKEGG <- function(df = NULL, type = "double", ntop = 20, color_by = "Direction", point_size = 3, text_size = 15){
+
+  # SETUP
+  ## setup direction and selected p-values
+
+  # first column are positive pv, second column are negative pv
+  tmp <- cbind(abs(df$logP.Up), abs(df$logP.Down))
+  idxMax <- apply(tmp, MARGIN = 1, which.max)
+  direction <- rep(NA, nrow(tmp))
+  direction[(idxMax == 1)] <- "up"
+  direction[(idxMax == 2)] <- "down"
+
+  selPV <- rep(NA, nrow(tmp))
+  selPV[(idxMax == 1)] <- tmp[(idxMax == 1),1]
+  selPV[(idxMax == 2)] <- -tmp[(idxMax == 2),2]
+
+  df <- df %>% mutate(Direction = direction)
+  df <- df %>% mutate(Selected_PV = selPV)
+
+  topKegg <- getTopKEGG(df = df, type = type, top.by = "Selected_PV", ntop = ntop)
+  topKegg <- sortDF(df = topKegg, sort.by = "Selected_PV", decreasing = FALSE)
+  levels <- topKegg$Pathway
+  topKegg[["Pathway"]] <- factor(topKegg[["Pathway"]], levels = levels)
+
+  p <- ggplot(topKegg, aes_string(x = "Selected_PV", y = "Pathway", color = color_by)) +
+    geom_point(size = point_size) +
+    theme_classic() +
+    labs(x = expression(paste('Signed ', -log[10],'(adjusted p-value)')),
+         y = "KEGG Pathways") +
+    TextSize(size = text_size)
+
+  return(p)
+}
